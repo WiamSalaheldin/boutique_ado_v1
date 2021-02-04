f88bbc5c3eafc6c5a094605b6522d78dfd3df038
WiamSalaheldin <wiam.khairelsid@gmail.com>
2021-02-04 22:11:54 +0000

    added stripe element

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IHFVDFipDkek1FsTf2qgKofjnz4ETcMzE5s0CwgN32YVXriItMOjNp1KLnY7QprBVMfRtzq4KwMPxrRHGEzNQ9G004CQbk8RQ',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
